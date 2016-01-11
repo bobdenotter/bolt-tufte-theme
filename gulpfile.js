5b6f9cad55bf2532fc586b1547aae6a8822f3da6
Bob den Otter <bobdenotter@gmail.com>
2016-01-11 17:17:47 +0100

    Switching from Grunt to Gulp.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var $    = require('gulp-load-plugins')();
+
+var sassPaths = [
+    'scss/',
+    'bower_components/foundation-sites/scss',
+    // 'bower_components/motion-ui/src',
+    'bower_components/slicknav/scss',
+    'bower_components/highlightjs/styles'
+];
+
+
+gulp.task('sass', function() {
+  return gulp.src('scss/tufte.scss')
+    .pipe($.sass({
+      includePaths: sassPaths
+    })
+      .on('error', $.sass.logError))
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie >= 9']
+    }))
+    .pipe(gulp.dest('css'));
+});
+
+
+gulp.task('default', ['sass'], function() {
+  gulp.watch(['scss/**/*.scss'], ['sass']);
+});
