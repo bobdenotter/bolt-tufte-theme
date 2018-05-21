4bd5a1598744e28644e2c375b94162b6081a3d8c
Bob den Otter <bob@twokings.nl>
2018-05-21 20:24:42 +0200

    Updating Bulma to 0.7.1, finetuning.

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -11,8 +11,27 @@ var sassPaths = [
 ];
 
 var javascriptFiles = [
-    'node_modules/jquery/dist/jquery.js',
+  'javascript/tufte.js',
+  'node_modules/jquery/dist/jquery.js',
+  // 'node_modules/baguettebox.js/src/baguetteBox.js',
+  'node_modules/prismjs/prism.js',
+  'node_modules/prismjs/components/prism-apacheconf.js',
+  'node_modules/prismjs/components/prism-bash.js',
+  'node_modules/prismjs/components/prism-css.js',
+  'node_modules/prismjs/components/prism-json.js',
+  'node_modules/prismjs/components/prism-markdown.js',
+  'node_modules/prismjs/components/prism-php.js',
+  'node_modules/prismjs/components/prism-python.js',
+  'node_modules/prismjs/components/prism-sass.js',
+  'node_modules/prismjs/components/prism-twig.js',
+  'node_modules/prismjs/components/prism-yaml.js',
+  'node_modules/prismjs/components/prism-markup-templating.js',
+  'node_modules/prismjs/plugins/line-numbers/prism-line-numbers.js',
+  'node_modules/prismjs/plugins/line-highlight/prism-line-highlight.js',
+  'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
 ];
+
+
 
 // Set up 'sass' task.
 gulp.task('sass', function() {
@@ -33,11 +52,11 @@ gulp.task('sass', function() {
 
 // Set up 'compress' task.
 gulp.task('compress', function() {
-  return gulp.src('javascript/*.js')
+  return gulp.src(javascriptFiles)
     .pipe($.if(PRODUCTION, $.uglify()))
+    .pipe($.concat('tufte.js'))
     .pipe(gulp.dest('../javascript'));
 });
-
 
 gulp.task('copyjavascript', function() {
    gulp.src(javascriptFiles)
@@ -49,10 +68,10 @@ gulp.task('setproduction', function() {
 });
 
 // Set up 'default' task, with watches.
-gulp.task('default', ['copyjavascript', 'sass', 'compress'], function() {
+gulp.task('default', ['sass', 'compress'], function() {
   gulp.watch(['scss/**/*.scss'], ['sass']);
   gulp.watch(['javascript/**/*.js'], ['compress']);
 });
 
 // Set up 'build' task, without watches and force 'production'.
-gulp.task('build', ['setproduction', 'copyjavascript', 'sass', 'compress']);
+gulp.task('build', ['setproduction','sass', 'compress']);
